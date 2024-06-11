934c7a295c3599b9db98b921e0e11617212c2d02
Jhon Mantilla <kaiser_4tu@hotmail.com>
2024-06-11 14:11:44 -0500

    Ajuste con los estilos del curso

--- app/src/main/java/com/example/superheroes/MainActivity.kt
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -13,11 +13,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -25,8 +30,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -62,7 +70,7 @@ fun SuperHeroApp(){
     ) { it ->
         LazyColumn(contentPadding = it) {
             items(heroes) {
-                HeroItem(hero = it, modifier = Modifier.padding(8.dp))
+                HeroItem(hero = it, modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp))
             }
         }
     }
@@ -73,15 +81,21 @@ fun HeroItem(
     hero: Hero,
     modifier: Modifier = Modifier
 ){
-    Card(modifier = modifier) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        modifier = modifier
+    ) {
         Row(
-            modifier = modifier
-                .fillMaxSize()
+            modifier = Modifier
+                .fillMaxWidth()
                 .padding(16.dp)
+                .sizeIn(minHeight = 72.dp)
         ) {
-            HeroInfo(heroName = hero.nameRes, heroDescription = hero.descriptionRes)
-            Spacer(modifier = Modifier.weight(1f))
-            HeroIcon(heroIcon = hero.imageRes)
+            HeroInfo(heroName = hero.nameRes, heroDescription = hero.descriptionRes, modifier = Modifier.weight(1f))
+            Spacer(modifier = Modifier.width(16.dp))
+            HeroIcon(heroIcon = hero.imageRes, modifier = Modifier
+                .size(72.dp)
+                .clip(RoundedCornerShape(8.dp)))
         }
     }
 
@@ -92,7 +106,7 @@ fun HeroInfo(
     @StringRes heroDescription: Int,
     modifier: Modifier = Modifier
 ){
-Box (modifier = modifier.size(width = 250.dp, height = 72.dp)) {
+//Box (modifier = modifier.size(width = 250.dp, height = 72.dp)) {
     Column (modifier = modifier) {
         Text(
             text = stringResource(id = heroName),
@@ -100,14 +114,11 @@ Box (modifier = modifier.size(width = 250.dp, height = 72.dp)) {
         )
         Text(
             text = stringResource(id =heroDescription),
-            style = MaterialTheme.typography.bodyLarge,
-            modifier = Modifier
+            style = MaterialTheme.typography.bodyLarge
 
         )
     }
-}
-
-
+//}
 
 }
 
@@ -116,13 +127,16 @@ fun HeroIcon(
     @DrawableRes heroIcon: Int,
     modifier: Modifier = Modifier
 ){
-Box {
+Box(
+    modifier = modifier
+)
+{
     Image(
-        modifier = modifier
-            .size(64.dp)
-            .aspectRatio(1f),
+        modifier = modifier,
         painter = painterResource(id = heroIcon),
-        contentDescription = null
+        contentDescription = null,
+        alignment = Alignment.TopCenter,
+        contentScale = ContentScale.FillWidth
     )
 }
 
